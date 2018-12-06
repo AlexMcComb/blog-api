e234abb8bd03d2489f10f80b4dae844db2ac97a1
Alex <alex@Alexs-MacBook-Pro.local>
2018-12-06 13:29:32 -0700

    reconfig app

--- models.js
+++ models.js
@@ -1,99 +1,33 @@
-const uuid = require('uuid');
+'use strict';
 
-// This module provides volatile storage, using a `BlogPost`
-// model. We haven't learned about databases yet, so for now
-// we're using in-memory storage. This means each time the app stops, our storage
-// gets erased.
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
-const mongoose = require("mongoose");
-
-// this is our schema to represent a restaurant
-const blogSchema = mongoose.Schema({
-  id: { type: String},
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  author: [
-    { 
-      firstName: String, required: true,
-      lastName: String, required: true
-     }],
-  publishDate: { type: Date, required: true }
+const blogPostSchema = mongoose.Schema({
+  author: {
+    firstName: String,
+    lastName: String
+  },
+  title: {type: String, required: true},
+  content: {type: String},
+  created: {type: Date, default: Date.now}
 });
 
-blogSchema.virtual("nameString").get(function() {
+
+blogPostSchema.virtual('authorName').get(function() {
   return `${this.author.firstName} ${this.author.lastName}`.trim();
 });
 
-blogSchema.methods.serialize = function() {
+blogPostSchema.methods.serialize = function() {
   return {
     id: this._id,
-    title: this.title,
+    author: this.authorName,
     content: this.content,
-    author: this.nameString,
-    publishDate: this.publishDate,
+    title: this.title,
+    created: this.created
   };
 };
 
-function StorageException(message) {
-   this.message = message;
-   this.name = "StorageException";
-}
+const BlogPost = mongoose.model('BlogPost', blogPostSchema);
 
-const BlogPosts = {
-  create: function(title, content, author, publishDate) {
-    console.log('Creating new blog post item');
-    const post = {
-      id: uuid.v4(),
-      title: title,
-      content: content,
-      author: author,
-      publishDate: publishDate || Date.now()
-    };
-    this.posts.push(post);
-    return post;
-  },
-  get: function(id=null) {
-    // if id passed in, retrieve single post,
-    // otherwise send all posts.
-    if (id !== null) {
-      return this.posts.find(post => post.id === id);
-    }
-    // return posts sorted (descending) by
-    // publish date
-    return this.posts.sort(function(a, b) {
-      return b.publishDate - a.publishDate
-    });
-  },
-  delete: function(id) {
-    const postIndex = this.posts.findIndex(
-      post => post.id === id);
-    if (postIndex > -1) {
-      this.posts.splice(postIndex, 1);
-    }
-  },
-  update: function(updatedPost) {
-    const {id} = updatedPost;
-    const postIndex = this.posts.findIndex(
-      post => post.id === updatedPost.id);
-    if (postIndex === -1) {
-      throw new StorageException(
-        `Can't update item \`${id}\` because doesn't exist.`)
-    }
-    this.posts[postIndex] = Object.assign(
-      this.posts[postIndex], updatedPost);
-    return this.posts[postIndex];
-  }
-};
-
-function createBlogPostsModel() {
-  const storage = Object.create(BlogPosts);
-  storage.posts = [];
-  return storage;
-}
-
-
-// note that all instance methods and virtual properties on our
-// schema must be defined *before* we make the call to `.model`.
-const Blog = mongoose.model("Blog", restaurantSchema);
-
-module.exports = {BlogPosts: createBlogPostsModel()};
+module.exports = {BlogPost};
